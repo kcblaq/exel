497c62985a9e7ce79dbde00b1a40b0be260222f1
Kelechi Ugwu <kcblack22@gmail.com>
2025-07-15 08:08:24 +0100

    Update on umbrella animation

--- src/components/home/animation-components/UmbrellaServices.tsx
+++ src/components/home/animation-components/UmbrellaServices.tsx
@@ -1,13 +1,70 @@
 "use client";
-import React from 'react';
-import animationData from '../../../../public/animations/umbrella-services.json';
-import Lottie from 'lottie-react';
+import React, { useEffect, useRef, useState } from 'react';
+import animationData from '../../../../public/animations/umbrella-services-re.json';
+import Lottie, { LottieRefCurrentProps } from 'lottie-react';
+
+// const UmbrellaServices = () => {
+//   return (
+//     <div className="jitter-container">
+//       <Lottie
+//         animationData={animationData}
+//         rendererSettings={{
+//           filterSize: {
+//             width: '200%',
+//             height: '200%',
+//             x: '-50%',
+//             y: '-50%',
+//           }
+//         }}
+//         className="w-full h-[50vh]"
+//       />
+//     </div>
+//   );
+// };
+
 
 const UmbrellaServices = () => {
+  const containerRef = useRef<HTMLDivElement | null>(null);
+  const lottieRef = useRef<LottieRefCurrentProps | null>(null);
+  const [inView, setInView] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setInView(entry.isIntersecting);
+      },
+      {
+        threshold: 0.5,
+      }
+    );
+
+    const currentContainer = containerRef.current;
+
+    if (currentContainer) {
+      observer.observe(currentContainer);
+    }
+
+    return () => {
+      if (currentContainer) observer.unobserve(currentContainer);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (inView) {
+      lottieRef.current?.play();
+    } else {
+      lottieRef.current?.stop();
+      lottieRef.current?.goToAndStop(0, true);
+    }
+  }, [inView]);
+
   return (
-    <div className="jitter-container">
+    <div ref={containerRef} className="jitter-container">
       <Lottie
+        lottieRef={lottieRef}
         animationData={animationData}
+        autoplay={false}
+        loop={true}
         rendererSettings={{
           filterSize: {
             width: '200%',
@@ -21,6 +78,5 @@ const UmbrellaServices = () => {
     </div>
   );
 };
-
 
 export default UmbrellaServices;
